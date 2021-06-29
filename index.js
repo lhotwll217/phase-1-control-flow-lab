f56a197492565a313e5ddeb7b290bb02800332cd
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-29 11:00:28 -0400

    commit tree 473bd7e9c655aaad903414c098ca8e209c02beea

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(fare) {
+  if (fare <= 400) {
+    return ('This one is on me!');
+  } else if (fare > 2000 && fare < 2500) {
+    return ('I will gladly take your thirty bucks.');
+  } else if (fare > 2500) {
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return ("Ok, sounds good.")
+  }  // Write your code here!
+
+  if (city !== 'NYC') {
+    return ('No go.')
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return ("Thank you so much.")
+  }
+  if (tip === 'not as generous') {
+    return ("Thank you.")
+  }
+  else
+    return ("Bye.")
   // Write your code here!
 }
